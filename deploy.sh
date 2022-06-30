be1da7a9bdc522fed8d86e7aad9d994d6a9857a0
Carlos Oliveira <carlos.oliveira@jumia.com>
2022-06-30 14:59:55 +0100

    Remove digital ocean token

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 docker-machine create --driver digitalocean \
  --digitalocean-image ubuntu-20-04-x64 \
- --digitalocean-access-token dop_v1_2e687fac41075c51ca261ff919529c0d4cb3726f3d4625d805ef3b5c483efeda \
+ --digitalocean-access-token <TOKEN> \
  --digitalocean-region fra1 \
  --engine-install-url "https://releases.rancher.com/install-docker/19.03.9.sh" \
  mypointi-wordpress-react-live
